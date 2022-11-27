fa8e349603c36a5274fc15c26213d5f4a0ee543b
ChenMingfeng <346776813@qq.com>
2022-11-27 23:18:43 +0800

    Update stroke.py
    Update file path.

--- stroke.py
+++ stroke.py
@@ -2,22 +2,23 @@
 """
 Created on Sun Nov 27 17:10:13 2022
 
-@author: ChenMingfeng
+@author: ChenMingfeng & RenChengwei
 """
 
 import pandas as pd
 
 if __name__ == "__main__":
-    X = pd.read_csv(
-        "C:/Users/ChenMingfeng/Downloads/documents-export-2022-11-27/1.Stroke prediction/Stroke_prediction_system_Data/Prf_feature_train.csv").iloc[:, 1:]
-    y = pd.read_csv(
-        "C:/Users/ChenMingfeng/Downloads/documents-export-2022-11-27/1.Stroke prediction/Stroke_prediction_system_Data/Stroke_label_train.csv").iloc[:,1:]
-    test_features = pd.read_csv(
-        "C:/Users/ChenMingfeng/Downloads/documents-export-2022-11-27/1.Stroke prediction/Stroke_prediction_system_Data/Prf_feature_test.csv").iloc[:, 1:]
-    print(X.shape)
-    print(y.shape)
+    path = "C:/Users/ChenMingfeng/Downloads/documents-export-2022-11-27/1.Stroke prediction/Stroke_prediction_system_Data/"
+    train_feature_path = path + "Prf_feature_train.csv"
+    train_label_path = path + "Stroke_label_train.csv"
+    test_feature_path = path + "Prf_feature_test.csv"
+
+    train_features = pd.read_csv(train_feature_path).iloc[:, 1:]
+    train_labels = pd.read_csv(train_label_path).iloc[:, 1:]
+    test_features = pd.read_csv(test_feature_path).iloc[:, 1:]
+    print(train_features.shape)
+    print(train_labels.shape)
     print(test_features.shape)
-    
-    X = X.fillna(method='ffill')
-    
-    
+
+    train_fetures = train_features.fillna(method='ffill')
+    redundant_cols = []
