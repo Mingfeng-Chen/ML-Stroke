42bb1d890c33acdac0fba6b27f43953df294550d
ChenMingfeng <346776813@qq.com>
2022-11-28 20:14:59 +0800

    Update stroke.py

--- stroke.py
+++ stroke.py
@@ -8,20 +8,27 @@ Created on Sun Nov 27 17:10:13 2022
 import pandas as pd
 
 if __name__ == "__main__":
-    #读取csv文件
+    # 读取csv文件
     path = "C:/Users/ChenMingfeng/Downloads/documents-export-2022-11-27/1.Stroke prediction/Stroke_prediction_system_Data/"
     train_feature_path = path + "Prf_feature_train.csv"
     train_label_path = path + "Stroke_label_train.csv"
     test_feature_path = path + "Prf_feature_test.csv"
-    
-    #删去第一列
+
+    # 删去第一列
     train_features = pd.read_csv(train_feature_path).iloc[:, 1:]
     train_labels = pd.read_csv(train_label_path).iloc[:, 1:]
     test_features = pd.read_csv(test_feature_path).iloc[:, 1:]
     print(train_features.shape)
     print(train_labels.shape)
     print(test_features.shape)
-    
-    #数据预处理
+
+    # 数据预处理
     train_fetures = train_features.fillna(method='ffill')
-    redundant_cols = []
+    
+    redundant_cols = ['FMONTH', 'IDATE', 'IMONTH', 'IDAY', 'IYEAR', 'CTELENM1', 'SEQNO', 'CTELNUM1', 'NUMHHOL3', 'NUMPHON3',
+                      'CPDEMO1B', 'X_CLLCPWT', 'X_DUALUSE', 'X_DUALCOR', 'X_LLCPWT2', 'X_LLCPWT', 'CELPHON1']
+    for c in redundant_cols:
+        del train_features[c]
+    
+    print(train_features.dtypes)
+    #类型修改为object
